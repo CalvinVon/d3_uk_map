fc05240b3d3d37fd565526319c3103602caee384
Calvin_Zhehe_win <vip935721016@163.com>
2019-01-30 20:44:21 +0800

    feat Displaying Places

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ d3.json("/assets/uk_topo.json", function (error, uk) {
 
     svg.append("path")
         .datum(subunits)
+        .attr("class", "uk")
         .attr("d", path)
         .attr('fill', function (d, i) {
             return color(i);
@@ -56,4 +57,31 @@ d3.json("/assets/uk_topo.json", function (error, uk) {
         }))
         .attr("d", path)
         .attr("class", "subunit-boundary IRL");
+
+    // Displaying Places
+    svg.append("path")
+        .datum(topojson.feature(uk, uk.objects.places))
+        .attr("d", path)
+        .attr("class", "place");
+
+    svg.selectAll(".place-label")
+        .data(topojson.feature(uk, uk.objects.places).features)
+        .enter().append("text")
+        .attr("class", "place-label")
+        .attr("transform", function (d) {
+            return "translate(" + projection(d.geometry.coordinates) + ")";
+        })
+        .attr("dy", ".35em")
+        .text(function (d) {
+            return d.properties.name;
+        });
+
+    // adjust label aligning
+    svg.selectAll(".place-label")
+        .attr("x", function (d) {
+            return d.geometry.coordinates[0] > -1 ? 6 : -6;
+        })
+        .style("text-anchor", function (d) {
+            return d.geometry.coordinates[0] > -1 ? "start" : "end";
+        });
 });
